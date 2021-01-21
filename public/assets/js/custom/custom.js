94d9ae4386a42531b90c63b54757f5ee88b5f765
Muazzam Younus <muazzam.younus925@gmail.com>
2021-01-21 18:00:39 +0500

    Navigation Level 1 & 2 Design

--- public/assets/js/custom/custom.js
+++ public/assets/js/custom/custom.js
@@ -1,3 +1,9 @@
-$(document).ready(function () {
-  // $('html').removeClass(' eventlistener localstorage no-touchevents no-forcetouch ');
+$(window).scroll(function (e) {
+    e.stopPropagation();
+    $('.header-main.navbar').removeClass('stuck');
 });
+
+$(document).ready(function () {
+    // $('html').removeClass(' eventlistener localstorage no-touchevents no-forcetouch ');
+});
+
