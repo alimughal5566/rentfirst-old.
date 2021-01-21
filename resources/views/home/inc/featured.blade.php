97d223d9d8a755494d9bfda8d1d05d781f209cbe
muhammad ali <muhammad.ali@ivylabtech.com>
2021-01-21 18:03:35 +0500

    new pull

--- resources/views/home/inc/featured.blade.php
+++ resources/views/home/inc/featured.blade.php
@@ -4,7 +4,7 @@ if (!isset($cacheExpiration)) {
 }
 ?>
 @if (isset($featured) and !empty($featured) and !empty($featured->posts))
-	@include('home.inc.spacer')
+{{--	@include('home.inc.spacer')--}}
 	<div class="container">
 		<div class="col-xl-12 content-box layout-section">
 			<div class="row row-featured row-featured-category">
